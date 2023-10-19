1a19c24b64af14d62a0368d1ab3d1d6d172bdcb5
mejia-dev <aaron.mejia8484@gmail.com>
2023-10-19 15:46:38 -0700

    Finalize functionality.
    Co-authored-by: Daisy Orozco <daisyyy.ob@gmail.com>
    Co-authored-by: Hunter Empey <LizardMak@gmail.com>
    Co-authored-by: Grant Abel <gtabel88@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
 // Business Logic
-window.addEventListener("load", function() {
-  document.getElementById("triangleInput").addEventListener("submit", getTriangles);
-});
+
 
 function getTriangles(e) {
   e.preventDefault();
+  window.alert("test")
   let sideA = parseInt(document.getElementById("sideA").value);
   let sideB = parseInt(document.getElementById("sideB").value);
   let sideC = parseInt(document.getElementById("sideC").value);
@@ -12,7 +11,10 @@ function getTriangles(e) {
 }
 
 function calculateTriangles(sideA,sideB,sideC) {  
-if (sideA === sideB === sideC) { 
+  if ((sideA + sideB <= sideC) || (sideA + sideC <= sideB) || (sideC + sideB <= sideA)) {
+    document.getElementById("resultString").innerText = "That's not a triangle! :( ";
+  }
+  else if (sideA === sideB && sideA === sideC) { 
   document.getElementById("resultString").innerText = "Equilateral";
 } else if ((sideA === sideB) || (sideA === sideC) || (sideB === sideC)) {
   document.getElementById("resultString").innerText = "Isosceles";
@@ -23,4 +25,7 @@ if (sideA === sideB === sideC) {
 }
 }
 
+window.addEventListener("load", function() {
+  document.getElementById("triangleInput").addEventListener("submit", getTriangles)
+})
 // UI Logic
